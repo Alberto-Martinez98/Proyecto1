c7956124589593bdcc9022ce14181ae0feb8458e
Alberto-Martinez98 <60900962+Alberto-Martinez98@users.noreply.github.com>
2020-02-11 13:21:08 -0600

    Update acces.php

--- framework/loginAdmin/acces.php
+++ framework/loginAdmin/acces.php
@@ -1,11 +1,11 @@
 <?php
 $administrador=$_POST['administrador'];
-$contraseña=$_POST['contraseña'];
+$contraseña=$_POST['password'];
 
 
-if (($administrador == 'administrador') AND ($contraseña=='administrador')) {
+if (($administrador == 'administrador') AND ($contraseña== 'administrador')) {
+	header('Location: http://localhost:88/framework/Página-Pirma/ejemplo.html');
 
-	echo "Bienvenido al sistema";
 }else{
 	echo "error al ingresar verifica tus datos,<br> <a href='login.php'> volver al login </a>";
 }
